855a5cd4a4b3cca49d4eeb395d719f56f6e34c06
Je Remy <jc@dc9.co.uk>
2017-10-10 23:13:03 +0100

    Added 2008r2 HyperV builder + modified Rename VM scripts

--- playbooks/Renaming VM auto script on new host start.ps1
+++ playbooks/Renaming VM auto script on new host start.ps1
@@ -3,18 +3,18 @@ Select-Object VMName,IPAddresses
 
 #set-item wsman:\localhost\Client\TrustedHosts * -force
 
-$IPAddress = '192.168.2.29'
-$NewVMName = 'web2'
+$IPAddress = '192.168.2.61'
+$NewVMName = 'web4'
 
-#Invoke-Command -ComputerName $IPAddress -credential Administrator -scriptblock {param ($NewVMName, $IPAddress)
-#Rename-Computer -NewName $NewVMName -ComputerName $env:COMPUTERNAME
+Invoke-Command -ComputerName $IPAddress -credential dc9cloud2\Administrator -scriptblock {param ($NewVMName, $IPAddress)
+Rename-Computer -NewName $NewVMName -ComputerName $env:COMPUTERNAME
 
- #   } -ArgumentList $NewVMName,$IPAddress
+    } -ArgumentList $NewVMName,$IPAddress
 
  #Start-Sleep -s  5
 
 Invoke-Command -ComputerName $IPAddress -credential Administrator -scriptblock {param ($IPAddress,$NewVMName)
- Add-Computer -ComputerName $env:COMPUTERNAME -NewName 'web3' -DomainName dc9cloud2.co.uk -Credential dc9cloud2\Administrator -Restart
+ Add-Computer -ComputerName $env:COMPUTERNAME -NewName $NewVMName -DomainName dc9cloud2.co.uk -Credential dc9cloud2\Administrator -Restart
 } -ArgumentList $IPAddress,$NewVMName
 
     
