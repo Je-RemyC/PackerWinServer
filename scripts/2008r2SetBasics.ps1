b4221fd5a38942ab8e588d4b7e886212e76d078f
Je Remy <jc@dc9.co.uk>
2017-10-18 21:52:07 +0100

    New server basics

--- scripts/2008r2SetBasics.ps1
+++ scripts/2008r2SetBasics.ps1
@@ -27,10 +27,24 @@ Function DisableFW {
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 }
 
+## Set Static IP
+
+    function SetStaticIP {
+       
+            $wmi = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true'"
+            $wmi.EnableStatic("192.168.2.211", "255.255.255.0")
+            $wmi.SetGateways("192.168.2.100", 1)
+            $wmi.SetDNSServerSearchOrder("192.168.2.10")
+            
+            Write-Host "Setting static IP Address"
+        }
+
+SetStaticIP 
+
 DisableFW
 
 EnableRDP
 
-EnableRDP
+DisableIEESC
 
 
