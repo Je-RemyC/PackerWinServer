92fa4d9618b5f05d490b486541cd8c5632aa92c5
unknown <Administrator@DC9CLOUD.CO.UK>
2017-10-20 00:01:52 +0100

    updated server basics

--- scripts/2008r2SetBasics.ps1
+++ scripts/2008r2SetBasics.ps1
@@ -9,23 +9,25 @@ $SubKey.SetValue("IsInstalled",0,[Microsoft.Win32.RegistryValueKind]::DWORD)
 Write-Host "Successfully disabled IE ESC on $Computer"
 $SuccessComps += $Computer
 }
-catch {
-Write-Host "Failed to disable IE ESC on $Computer"
-$FailedComps += $Computer
-}
+
 
 ## Enable RDP
 
 Function EnableRDP {
-Reg add “\\$computername\HKLM\SYSTEM\CurentControlSet\Control\Terminal Server”  /v fDenyTSConnections /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINESYSTEM\CurrentControl\SetControl\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
 }
 
 
 ## Disable Firewall
 
-Function DisableFW {
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-}
+    function DisableFW   {
+
+           $FWState = "off"
+
+            netsh advfirewall set allprofiles state $FWState
+             
+            write-host "Disabling Firewall"
+       }  
 
 ## Set Static IP
 
@@ -34,7 +36,7 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
             $wmi = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true'"
             $wmi.EnableStatic("192.168.2.211", "255.255.255.0")
             $wmi.SetGateways("192.168.2.100", 1)
-            $wmi.SetDNSServerSearchOrder("192.168.2.10")
+            $wmi.SetDNSServerSearchOrder("192.168.2.100")
             
             Write-Host "Setting static IP Address"
         }
